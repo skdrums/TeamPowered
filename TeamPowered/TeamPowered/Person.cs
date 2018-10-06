6c68aa971b137f409f553ea12d6d6b3b908333b2
sora kikuchi <sora.kannaduki1009@gmail.com>
2018-10-06 09:24:39 +0900

    changed Firstname to Middlename

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -9,7 +9,7 @@ namespace TeamPowered
     class Person
     {
 
-        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
     }
